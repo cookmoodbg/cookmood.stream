7b2ad5fe49289337b1fdfb6847b55a4b6d139e9b
cookmoodbg <72431251+cookmoodbg@users.noreply.github.com>
2022-03-03 10:33:00 +0200

    Format, extract variables, add more lambda code

--- netlify/functions/form_contact.js
+++ netlify/functions/form_contact.js
@@ -1,4 +1,5 @@
-const sendMail = require('sendmail')()
+const sendMail = require('sendmail')
+const busboy = require('busboy')
 
 exports.handler = (event, context, callback) => {
   if (!process.env.CONTACT_EMAIL) {
@@ -8,7 +9,25 @@ exports.handler = (event, context, callback) => {
     })
   }
 
-  const body = JSON.parse(event.body)
+  const busboy = new Busboy({
+    headers: event.headers
+  });
+
+  const fields = {};
+
+  // whenever busboy comes across a normal field ...
+  busboy.on("field", (fieldName, value) => {
+    // ... we write its value into `fields`.
+    fields[fieldName] = value;
+  });
+
+  // once busboy is finished, we resolve the promise with the resulted fields.
+  busboy.on("finish", () => {
+    resolve(fields)
+  });
+
+  // now that all handlers are set up, we can finally start processing our request!
+  busboy.write(event.body);
 
   try {
     //validateLength('body.name', body.name, 3, 50)
@@ -41,10 +60,10 @@ exports.handler = (event, context, callback) => {
   }
 
   const descriptor = {
-    from: `"${body.email}" <no-reply@cookmood.stream>`,
+    from: `"${fields.email}" <no-reply@cookmood.stream>`,
     to: process.env.CONTACT_EMAIL,
-    subject: `${body.name} sent you a message from cookmood.stream`,
-    text: body.message,
+    subject: `${fields.name} sent you a message from cookmood.stream`,
+    text: fields.message,
   }
 
   sendMail(descriptor, (e) => {
