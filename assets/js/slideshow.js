3af8fcf692402b099a8575d45cba221f1926532b
cookmoodbg <72431251+cookmoodbg@users.noreply.github.com>
2022-03-16 14:52:58 +0200

    Add slideshow gallery

---
+++ assets/js/slideshow.js
@@ -0,0 +1,30 @@
+var slideshowController;
+
+function SlideshowController(imageClassSelector) {
+  var slideIndex = 1;
+  this.init = function () {
+    this.showDivs(slideIndex);
+  };
+  this.plusDivs = function (n) {
+    this.showDivs((slideIndex += n));
+  };
+  this.showDivs = function (n) {
+    var i;
+    var x = document.getElementsByClassName(imageClassSelector);
+    if (n > x.length) {
+      slideIndex = 1;
+    }
+    if (n < 1) {
+      slideIndex = x.length;
+    }
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+    }
+    x[slideIndex - 1].style.display = "block";
+  };
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  slideshowController = new SlideshowController("slideshowSlides");
+  slideshowController.init();
+});
