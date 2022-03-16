2b9377a9fa0afaf36caf94df964a2a31a1058aee
Cook Mood BG <72431251+cookmoodbg@users.noreply.github.com>
2022-03-16 17:17:00 +0200

    Add slideshow gallery (#7)
    * Add slideshow gallery
    
    * Add more imgs to initial article
    
    * ECMA 2015 module for slideshow
    
    * Remove wrong download link

---
+++ assets/js/slideshow.js
@@ -0,0 +1,23 @@
+export function SlideshowController(imageClassSelector) {
+  var slideIndex = 1;
+  this.init = function () {
+    this.showDivs(slideIndex);
+  };
+  this.plusDivs = function (n) {
+    this.showDivs((slideIndex += n));
+  };
+  this.showDivs = function (n) {
+    var i;
+    var x = document.getElementsByClassName(imageClassSelector);
+    if (n > x.length) {
+      slideIndex = 1;
+    }
+    if (n < 1) {
+      slideIndex = x.length;
+    }
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+    }
+    x[slideIndex - 1].style.display = "block";
+  };
+}
