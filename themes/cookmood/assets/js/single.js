62aa264297a4d4733ba8cdb509b6dc464ee60ebc
cookmoodbg <72431251+cookmoodbg@users.noreply.github.com>
2022-03-18 09:41:49 +0200

    Arrange single page js

---
+++ themes/cookmood/assets/js/single.js
@@ -0,0 +1,18 @@
+import { SlideshowController } from "./util/slideshow.js";
+
+document.addEventListener("DOMContentLoaded", function () {
+  let slideshowController = new SlideshowController("slideshowSlides");
+  slideshowController.init();
+
+  document
+    .getElementById("button_slideshow_left")
+    .addEventListener("click", function () {
+      slideshowController.plusDivs(-1);
+    });
+
+  document
+    .getElementById("button_slideshow_right")
+    .addEventListener("click", function () {
+      slideshowController.plusDivs(+1);
+    });
+});
